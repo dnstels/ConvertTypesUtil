f8500867c8988bf14f1345f39617bb3e4d779c46
Stels Stels <d_stels@mail.ru>
2023-11-23 14:00:52 +0400

    Переименование namespace

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-namespace ConvertUtils
+namespace ConvertTypesUtil
 {
     public static class StringExtensions
     {
